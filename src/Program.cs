039fefd9ab2048abbf3dc170f1aed3e90216078d
software-programmer <matt.g.parker@live.co.uk>
2025-04-27 19:52:17 +0100

    feat: minimal poc

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => $"Minimal API. {Environment.GetEnvironmentVariable("VERSION") ?? "Unknown Version"}");
 
 app.Run();
